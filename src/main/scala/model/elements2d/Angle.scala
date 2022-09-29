2233d53be320dee2e776cead51e237a6f4c966f7
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-09-29 09:19:56 +0200

    Angle refactored

--- src/main/scala/model/elements2d/Angle.scala
+++ src/main/scala/model/elements2d/Angle.scala
@@ -1,17 +1,42 @@
 package model.elements2d
 
+
+object Angle:
+  private val straightAngleRadiant = math.Pi
+  private val straightAngleDegree = 180
+
+  private def identity(value: Double): Double = value
+
+  private def periodicWithNegative(value: Double, straightAngle: Double): Double = -straightAngle + math.abs(value % straightAngle)
+
+  private def periodicCircular(value: Double, completeAngle: Double): Double = value % completeAngle
+
 enum Angle:
+
+  import Angle._
+
   case Degree(value: Double)
   case Radian(value: Double)
 
-  def radiant: Double = this match
-    case Degree(value) if value % 360 > 180 => (-180 + value % 180) * Math.PI / 180
-    case Degree(value) => value * Math.PI / 180
-    case Radian(value) if (value % (2 * Math.PI)) > Math.PI => -Math.PI + value % Math.PI
-    case Radian(value) => value
+  def radiant: Double =
+    def degreeToRadian(value: Double): Double = value * straightAngleRadiant / straightAngleDegree
 
-  def degree: Double = this match
-    case Degree(value) if value % 360 > 180 => -180 + value % 180
-    case Degree(value) => value
-    case Radian(value) if (value % (2 * Math.PI)) > Math.PI => -180 + ((value * 180 / Math.PI) % 180)
-    case Radian(value) => value * 180 / Math.PI
+    conversion(straightAngleRadiant)(degreeToRadian)(identity)
+
+  def degree: Double =
+    def radiantToDegree(value: Double): Double = value * straightAngleDegree / straightAngleRadiant
+
+    conversion(straightAngleDegree)(identity)(radiantToDegree)
+
+  private def conversion(straightAngleOfConversion: Double)(fromDegree: Double => Double)(fromRadian: Double => Double): Double =
+    def conversionPeriodicWithNegative(value: Double)(straightAngleOfConverted: Double)(conversionFunction: Double => Double): Double =
+      val completeAngleOfConverted = 2 * straightAngleOfConverted
+      val valuePeriodicCircular = periodicCircular(value, completeAngleOfConverted)
+      value match
+        case _ if valuePeriodicCircular == -straightAngleOfConverted => straightAngleOfConversion
+        case _ if valuePeriodicCircular > straightAngleOfConverted || valuePeriodicCircular < -straightAngleOfConverted => periodicWithNegative(conversionFunction(value), straightAngleOfConversion)
+        case _ => conversionFunction(valuePeriodicCircular)
+
+    this match
+      case Degree(value) => conversionPeriodicWithNegative(value)(straightAngleDegree)(fromDegree)
+      case Radian(value) => conversionPeriodicWithNegative(value)(straightAngleRadiant)(fromRadian)
