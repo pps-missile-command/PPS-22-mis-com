c60cb134e6974a00e87d1bc7aabeaf567f25d939
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-10-07 02:47:44 +0200

    Created HitBoxEmpty with its tests

---
+++ src/test/scala/model/collisions/HitBoxEmptyTest.scala
@@ -0,0 +1,34 @@
+package model.collisions
+
+import model.collisions.hitbox.HitBoxEmpty
+import model.elements2d.Point2D
+import org.scalatest.funspec.AnyFunSpec
+
+class HitBoxEmptyTest extends AnyFunSpec :
+
+  private val hitBoxEmpty = HitBoxEmpty
+
+  describe("An hit box") {
+
+    describe("that is empty") {
+
+      it("should have an empty iterator of points") {
+        assert(!hitBoxEmpty.iterator.hasNext)
+      }
+
+      it("should not contain any point") {
+        assert(!hitBoxEmpty.contains(Point2D(0, 0)))
+      }
+
+      it("should have a size of 0") {
+        assert(hitBoxEmpty.isEmpty)
+      }
+
+      it("shouldn't have a space of points ( no x and y interval)") {
+        assert(hitBoxEmpty.xMax == Option.empty)
+        assert(hitBoxEmpty.xMin == Option.empty)
+        assert(hitBoxEmpty.yMax == Option.empty)
+        assert(hitBoxEmpty.yMin == Option.empty)
+      }
+    }
+  }
