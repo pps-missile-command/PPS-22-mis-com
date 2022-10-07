a67b5a0e46e25242e0e1bf2a6c0ce8bf8232f401
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-10-07 02:50:52 +0200

    Created HitBoxPoint with its tests

---
+++ src/main/scala/model/collisions/hitbox/HitBoxPoint.scala
@@ -0,0 +1,35 @@
+package model.collisions.hitbox
+
+import model.collisions.HitBox
+import model.elements2d.Point2D
+import model.elements2d.Point2D.GivenEquality.given
+import org.scalactic.TripleEquals._
+import model.collisions._
+import org.scalactic.Equality
+
+/**
+ * Factory for an hit box that is a point.
+ */
+object HitBoxPoint:
+
+  /**
+   * Returns a new hit box that contains only a point.
+   *
+   * @param point the point in the hit box
+   * @return a new hit box that contains only a point
+   */
+  def apply(point: Point2D): HitBox = HitBoxPoint(point)
+
+  private case class HitBoxPoint(point: Point2D)(using step: Step = 0) extends HitBox :
+
+    override val xMax: Option[Double] = Option(point.x)
+
+    override val yMax: Option[Double] = Option(point.y)
+
+    override val xMin: Option[Double] = Option(point.x)
+
+    override val yMin: Option[Double] = Option(point.y)
+
+    override def iterator: Iterator[Point2D] = Iterator(point)
+
+    override def contains(point: Point2D)(using equality: Equality[Double]): Boolean = point === this.point
