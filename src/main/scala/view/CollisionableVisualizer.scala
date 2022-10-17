27ae6e7b69ea3d60227a115d169b9117259ad2ea
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-10-17 18:55:25 +0200

    Added new collisionable visualizer for view elements

---
+++ src/main/scala/view/CollisionableVisualizer.scala
@@ -0,0 +1,25 @@
+package view
+
+import javafx.scene.image.{Image, ImageView}
+import model.collisions.Collisionable
+import model.missile.{CollisionableElement, Missile, MissileImpl, hitboxBase, hitboxHeight}
+
+case class CollisionableElement(imageView: ImageView, x: Double, y: Double)
+
+object MissileView:
+  val resourceFolderPath: String = ???
+
+  val conversion: Collisionable => CollisionableElement = _ match
+    case m: Missile => CollisionableElement(
+      ImageView(Image(getClass.getResourceAsStream(""), hitboxBase, hitboxHeight, false, false)),
+      m.position.x,
+      m.position.y)
+
+  def apply(imageView: ImageView, x: Double, y: Double) =
+    CollisionableElement(imageView, x, y)
+
+  def printMissiles(collisionables: List[Collisionable]): List[CollisionableElement] =
+    for
+      i <- collisionables
+    yield conversion(i)
+
