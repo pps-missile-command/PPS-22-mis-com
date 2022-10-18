d72f77cf094d8c81813e48bef3f9926a3af3e3dd
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-10-18 18:50:07 +0200

    Updated spawner object extending Timeable

--- src/main/scala/model/spawner/Spawner.scala
+++ src/main/scala/model/spawner/Spawner.scala
@@ -1,9 +1,12 @@
 package model.spawner
 
+import model.behavior.*
 import model.collisions.Affiliation
 import model.elements2d.Point2D
-import model.missile.{DeltaTime, Missile, damage, initialLife, velocity}
+import model.missile.{Missile, damage, initialLife, velocity}
 import model.missile.Missile
+import model.DeltaTime
+
 import scala.util.Random
 
 /**
@@ -17,33 +20,36 @@ import scala.util.Random
  * classe lista di missili
  */
 
-trait Spawner(using Random):
+trait Spawner(using Random) extends Timeable:
 
   def spawn(dt: DeltaTime): List[Missile]
-
-  def changeInterval(newInterval: DeltaTime, spawner: Spawner): Spawner
 
 object Spawner:
 
   given affiliation: Affiliation = Affiliation.Enemy
 
-  def apply(interval: DeltaTime, maxWidth: Double, maxHeight: Double) = new Spawner(using Random) {
+  def apply(interval: DeltaTime, maxWidth: Double, maxHeight: Double, dt: DeltaTime = 0): Spawner = new Spawner(using Random) {
 
-    override def spawn(dt: DeltaTime): List[Missile] = dt match
-      case n if n >= interval =>
-        val step: Int = (n / interval).toInt
-        val randomX = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
-        val randomY = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
-        val generator: List[Missile] =
-          for
-            (i, x) <- randomX
-            (j, y) <- randomY
-            if i == j
-          yield Missile.enemyMissile(initialLife, damage, velocity, Point2D(x, y), Point2D(x, y))
-        generator
-      case _ => List()
+    var dtSupplier : () => DeltaTime = () => dt
 
-    override def changeInterval(newInterval: DeltaTime, spawner: Spawner) = ???
+    override def spawn(dt: DeltaTime): List[Missile] =
+      val currentDt = dtSupplier()
+      this.dtSupplier = () => 0
+      currentDt match
+        case n if n >= interval =>
+          val step: Int = (n / interval).toInt
+          val randomX = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
+          val randomY = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
+          val generator: List[Missile] =
+            for
+              (i, x) <- randomX
+              (j, y) <- randomY
+              if i == j
+            yield Missile.enemyMissile(initialLife, damage, velocity, Point2D(x, y), Point2D(x, y))
+          generator
+        case _ => List()
+
+    override def timeElapsed(_dt: DeltaTime): Spawner = Spawner.apply(interval, maxWidth, maxHeight, dtSupplier() + _dt)
 
   }
 
