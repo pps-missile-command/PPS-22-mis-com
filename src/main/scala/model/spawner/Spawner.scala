ef0af43048429b7aa67b1931f0993eadd4c362f4
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-10-22 17:09:05 +0200

    Fixed spawner class with timeable extension

--- src/main/scala/model/spawner/Spawner.scala
+++ src/main/scala/model/spawner/Spawner.scala
@@ -13,13 +13,15 @@ trait Spawner(using Random) extends Timeable:
 
   def spawn(dt: DeltaTime): List[Missile]
 
+  override def timeElapsed(dt: DeltaTime): Spawner
+
 object Spawner:
 
   given affiliation: Affiliation = Affiliation.Enemy
 
   def apply(interval: DeltaTime, maxWidth: Double, maxHeight: Double, dt: DeltaTime = 0): Spawner = new Spawner(using Random) {
 
-    var dtSupplier : () => DeltaTime = () => dt
+    private var dtSupplier : () => DeltaTime = () => dt
 
     override def spawn(dt: DeltaTime): List[Missile] =
       val currentDt = dtSupplier()
@@ -27,23 +29,17 @@ object Spawner:
       dt match
         case n if n >= interval =>
           val step: Int = (n / interval).toInt
-          val randomX = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
-          val randomY = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
+          val randomX_start = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
+          val randomX_end = (0 until step map { i => (i, Random.nextDouble() * maxWidth) }).toList
           val generator: List[Missile] =
             for
-              (i, x) <- randomX
-              (j, y) <- randomY
+              (i, x_start) <- randomX_start
+              (j, x_end) <- randomX_end
               if i == j
-            yield Missile.enemyMissile(initialLife, damage, velocity, Point2D(x, y), Point2D(x, y))
+            yield Missile.enemyMissile(initialLife, damage, velocity, Point2D(x_start, maxHeight), Point2D(x_end, 0)) //TODO coordinate campo
           generator
         case _ => List()
 
     override def timeElapsed(_dt: DeltaTime): Spawner = Spawner.apply(interval, maxWidth, maxHeight, dtSupplier() + _dt)
 
-  }
-
-  @main def test(): Unit = {
-    val spawner = Spawner(5, 10, 10)
-
-    println(spawner.spawn(10))
   }
