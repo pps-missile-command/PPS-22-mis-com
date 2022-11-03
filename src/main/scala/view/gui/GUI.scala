26712b59e9c018df116587a4642efb7e3def74b4
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-11-03 18:26:17 +0100

    Fixed minor things

--- src/main/scala/view/gui/GUI.scala
+++ src/main/scala/view/gui/GUI.scala
@@ -7,13 +7,11 @@ import monix.reactive.Observable
 import monix.reactive.subjects.PublishSubject
 import org.w3c.dom.events.MouseEvent
 import view.gui.UI
-
 import model.elements2d.Point2D
-
 import controller.Event
 
 import java.awt.event.MouseMotionListener
-import java.awt.{Color, Graphics, event}
+import java.awt.{BorderLayout, Color, Dimension, Graphics, event, FlowLayout}
 import javax.swing.*
 
 class GUI(width: Int, height: Int) extends UI:
@@ -44,16 +42,24 @@ class GUI(width: Int, height: Int) extends UI:
 
     override def events: Observable[Event] = frame.getContentPane
       .mouseObservable()
-      .map((x, y) => (x / width.toDouble, y / height.toDouble))
       .map((x, y) => Event.LaunchMissileTo(Point2D(x, y)) )
 
     override def gameOver: Task[Unit] = Task {
+        gameOverRender
+    }
+    
+    def gameOverRender: Task[Unit] = Task {
+
         SwingUtilities.invokeAndWait { () =>
             if (frame.getContentPane.getComponentCount != 0)
-                0 to (frame.getContentPane.getComponentCount - 1) foreach { i=>
-                    frame.getContentPane.remove(i)
-                }
-            frame.getContentPane.add(EndGamePane(width, height))
+                frame.getContentPane.remove(0)
+            val panel = new JPanel()
+            panel.setSize(width, height)
+            panel.setLayout(new FlowLayout())
+            val button = new JButton()
+            button.setText("RIGIOCA")
+            panel.add(button)
+            frame.getContentPane.add(panel)
             frame.getContentPane.repaint()
         }
     }
