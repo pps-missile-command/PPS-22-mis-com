e922d77add34213d26f09e38d3b4b4ffb5658a26
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-11-19 18:34:29 +0100

    Added new world to view coordinates mapping

--- src/main/scala/view/CollisionableVisualizer.scala
+++ src/main/scala/view/CollisionableVisualizer.scala
@@ -1,5 +1,7 @@
 package view
 
+import model.World
+
 import java.awt.{Color, Image, Toolkit}
 import model.collisions.{Affiliation, Collisionable}
 import model.elements2d.{Angle, Point2D}
@@ -10,10 +12,15 @@ import java.awt.image.BufferedImage
 import javax.imageio.ImageIO
 import java.io.File
 
+extension(p: Point2D)
+  def map(f: (Point2D) => Point2D) = f(p)
+
 case class CollisionableElement(image: BufferedImage, baseWidth: Int, baseHeight: Int,
                                 position: Point2D, angle: Angle = Angle.Degree(0))
 
 object CollisionableVisualizer:
+
+  private val convertPosition: (Point2D) => Point2D = (p) => Point2D((p.x * ViewConstants.GUI_width) / World.width, (p.y * ViewConstants.GUI_height) / World.height)
 
   def printElements(collisionables: List[Collisionable])(using conversion: Conversion[Double, Int]): List[CollisionableElement] =
 
@@ -25,7 +32,7 @@ object CollisionableVisualizer:
             case Affiliation.Enemy => optImage = Option(ImageIO.read(getClass.getResource("/enemy_missile.png")))
             case _ => optImage = Option(ImageIO.read(getClass.getResource("/friendly_missile.png")))
         }
-        CollisionableElement(optImage.getOrElse(null), hitboxBase, hitboxHeight, m.position, m.angle.getOrElse(Angle.Degree(0)))
+        CollisionableElement(optImage.getOrElse(null), hitboxBase, hitboxHeight, m.position map convertPosition, m.angle.getOrElse(Angle.Degree(0)))
       case e: Explosion =>
         val diameter = e.radius * 2
         val bi = new BufferedImage(diameter,diameter, BufferedImage.TYPE_INT_ARGB)
@@ -33,7 +40,7 @@ object CollisionableVisualizer:
         g2d.setColor(Color.RED)
         g2d.drawOval(0, 0, diameter, diameter)
         g2d.dispose()
-        CollisionableElement(bi, diameter, diameter, e.position)
+        CollisionableElement(bi, diameter, diameter, e.position map convertPosition)
 
     collisionables.map(conversion)
 
