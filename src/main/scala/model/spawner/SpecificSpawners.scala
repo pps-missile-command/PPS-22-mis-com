91fc821abfb3fb1ea5ab132e85ceeeae8d13e3b3
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-11-19 18:28:54 +0100

    Added new specific spawner

--- src/main/scala/model/spawner/SpecificSpawners.scala
+++ src/main/scala/model/spawner/SpecificSpawners.scala
@@ -1,15 +1,17 @@
 package model.spawner
 
 import model.elements2d.Point2D
+import model.missile
 import model.missile.Missile
+import model.missile.zigzag.ZigZagMissile.ZigZagMissile
 import model.spawner.LazySpawner.Spawnable
 
 import scala.util.Random
 
 extension(r: Random)
   def randomPosition(maxWidth: Double, maxHeight: Double): Point2D = Point2D(r.nextDouble() * maxWidth, r.nextDouble() * maxHeight)
-  def randomX(maxWidth: Double): Point2D = Point2D(r.nextDouble() * maxWidth, 0)
-  def randomY(maxHeight: Double): Point2D = Point2D(0, r.nextDouble() * maxHeight)
+  def randomX(maxWidth: Double, y: Double): Point2D = Point2D(r.nextDouble() * maxWidth, y)
+  def randomY(maxHeight: Double, x: Double): Point2D = Point2D(x, r.nextDouble() * maxHeight)
 
 /**
  * Specific spawners strategies to spawn objects
@@ -19,5 +21,8 @@ extension(r: Random)
 object SpecificSpawners:
 
   def MissileStrategy(maxWidth: Double, maxHeight: Double)(using r: Random): Spawnable[Missile] =
-    () => Missile.enemyMissile(position = r.randomX(maxWidth), finalPosition = r.randomX(maxHeight))
+    () => Missile.enemyMissile(position = r.randomX(maxWidth, 0), finalPosition = r.randomX(maxWidth, maxHeight))
+
+  def ZigZagStrategy(maxWidth: Double, maxHeight: Double)(using r: Random): Spawnable[Missile] =
+    () => missile.zigzag.ZigZagMissile(r.randomX(maxWidth, 0), r.randomX(maxWidth, maxHeight), 5)
     
