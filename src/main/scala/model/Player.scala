1627b6c76df0a700f40079777868f031c08ea02f
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-11-21 20:09:38 +0100

    Created class Player

---
+++ src/main/scala/model/Player.scala
@@ -0,0 +1,24 @@
+package model
+
+import model.Timer.Timer
+
+/**
+ * Class that represents a player in the game.
+ *
+ * @param score the score of the player
+ * @param timer the surviving timer of the player
+ */
+case class Player(score: ScorePoint, timer: Timer)
+
+/**
+ * Companion object of the Player class.
+ */
+object Player:
+
+  /**
+   * The initial player.
+   *
+   * @return the initial player
+   */
+  def initialPlayer(): Player =
+    Player(0, Timer(0))
