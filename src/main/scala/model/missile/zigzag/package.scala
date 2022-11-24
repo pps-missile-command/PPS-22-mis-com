e8bd931c0ac6457cc61c05217f7c7480669af206
Daniele Di Lillo <daniele.dilillo@studio.unibo.it>
2022-11-24 22:11:45 +0100

    Test fix

--- src/main/scala/model/missile/zigzag/package.scala
+++ src/main/scala/model/missile/zigzag/package.scala
@@ -50,6 +50,9 @@ package object zigzag:
    * adjust the x component if the point is out of bound
    */
   extension(p: Point2D)
+    def ~=(p1: Point2D): Boolean =
+      (Math.round(p.x * 100) / 100) == (Math.round(p1.x * 100) / 100) && (Math.round(p.y * 100) / 100) == (Math.round(p1.y * 100) / 100)
+
     def filterMap(maxWidth: Double): Point2D = p.x match
       case n if n > maxWidth => Point2D(maxWidth, p.y)
       case n if n < 0 => Point2D(0, p.y)
