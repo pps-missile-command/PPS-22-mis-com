e7faf2339066fc7fc4ffa922e3802c8667b8536f
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-11-30 09:22:59 +0100

    Features In CakePattern remove main and used in Main

--- src/main/scala/components/CakePattern.scala
+++ src/main/scala/components/CakePattern.scala
@@ -1,15 +1,10 @@
 package components
 
-import view.gui.UI
-
 object MVC
   extends ModelModule.Interface
   with ViewModule.Interface
   with ControllerModule.Interface:
 
   override val model: ModelModule.Model = new ModelImpl()
-  override val view: UI & ViewModule.View = new ViewImpl()
-  override val controller: ControllerModule.MainController = new ControllerImpl()
-
-  @main def main(): Unit =
-    controller.initializeGame()
+  override val view: ViewModule.View = new ViewImpl()
+  override val controller: ControllerModule.Controller = new ControllerImpl()
