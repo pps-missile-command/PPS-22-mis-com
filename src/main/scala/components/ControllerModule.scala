6354c224e93fc0b6ca5b75a0760ecb1c40218f72
Andrea Brighi <andrea.brighi8@studio.unibo.it>
2022-11-30 09:22:14 +0100

    Features In ControllerModule refactor

--- src/main/scala/components/ControllerModule.scala
+++ src/main/scala/components/ControllerModule.scala
@@ -1,22 +1,20 @@
 package components
 import monix.execution.Scheduler.Implicits.global
 import monix.reactive.{Observable, OverflowStrategy}
-import controller.Controller
+import controller.GameController
 
 object ControllerModule:
-  trait MainController:
+  trait Controller:
     def startGame(): Unit
-    def initializeGame(): Unit
   trait Provider:
-    val controller: MainController
+    val controller: Controller
   type Requirements = ViewModule.Provider with ModelModule.Provider
   trait Component:
     context: Requirements =>
-    class ControllerImpl extends MainController:
+    class ControllerImpl extends Controller:
+      override def startGame(): Unit =
+        GameController(view, model).start().runAsyncAndForget
 
-      override def startGame(): Unit = Controller.start(view).runAsyncAndForget
-
-      override def initializeGame(): Unit = view.initialGame()
 
   trait Interface extends Provider with Component:
     self: Requirements =>
